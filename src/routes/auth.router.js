34e68599a9561cf32a4ac6720219b444a21ce9bb
Yaswanth <yashwanth.k@mooo.farm>
2022-11-16 03:27:32 +0530

    static pages

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 const { OtpController, AuthController } = require("../controllers");
 
 router.post("/login", AuthController.POST_login);
 router.post("/sendOtp", OtpController.POST_sendOtp);
 router.post("/verifyOtp", OtpController.POST_verifyOtp);
 router.post("/deleteAccount", AuthController.POST_deleteAccount);
+router.get("/terms_and_conditions", function (req, res) {
+  res.sendFile(path.join(__dirname + "/T&C.html"));
+});
+router.get("/privacy_policy", function (req, res) {
+  res.sendFile(path.join(__dirname + "/privacyPolicy.html"));
+});
 
 module.exports = router;
